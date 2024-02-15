a0281cd4a5a6c4b4e4a0e8fc86da0d5206c298a1
Tristan Dasilva <67392244+tristandasilva@users.noreply.github.com>
2024-02-14 21:51:06 -0500

    Styling testimonial slider

--- app/components/Testimonials/Testimonial.jsx
+++ app/components/Testimonials/Testimonial.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Testimonial = ({ testimonial }) => {
   return (
-    <div className='p-10 max-w-[80%] border font-sans text-gray-700'>
-      <div>Quotation</div>
-      <div>{testimonial.text}</div>
-      <div>&#8212;&#8212; {testimonial.clientName}</div>
+    <div className='p-14 md:max-w-[80%] font-sans text-gray-700 dropShadow'>
+      <div className='-pr-4'>
+        <Image
+          src={'/quotation.svg'}
+          alt='Quotation mark'
+          width={80}
+          height={80}
+        />
+      </div>
+      <div className='flex justify-center -mt-7'>
+        <div className='flex flex-col gap-4 max-w-[90%]'>
+          <div className='md:text-[17px]'>{testimonial.text}</div>
+          <div className='text-[15px] font-bold text-darker-blue'>
+            &#8212;&#8212; {testimonial.clientName}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
